aecac2c21a542aa8941c77ebeafde837bd2d2792
tomkur7 <b777tom@gmail.com>
2017-11-08 19:46:26 +0700

    first commit

---
+++ Mycommers/database/migrations/2017_10_25_121624_CreateCategoryTable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        schema::create('categories', function (blueprint $table) {
+            $table->increments('id');
+            $table->string('categoryName');
+            $table->string('categoryImage');
+            $table->timestamps();
+
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        schema::dropIfExists('categories');
+    }
+}
